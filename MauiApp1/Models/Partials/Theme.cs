1fee2338f0ee089ed34aa34d1b78f324974c5dc1
Aidar <ajdar.mar@yandex.ru>
2024-10-31 20:49:45 +0300

    Add Models

---
+++ MauiApp1/Models/Partials/Theme.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public partial class Theme
+    {
+        private bool isExpanded;
+        public bool IsExpanded { get; set; } = false;
+        //{
+        //    get => isExpanded;
+        //    set
+        //    {
+        //        if (isExpanded != value)
+        //        {
+        //            isExpanded = value;
+        //            OnPropertyChanged(nameof(IsExpanded));
+        //        }
+        //    }
+        //}
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //protected virtual void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+    }
+}
---
+++ MauiApp1/Models/Theme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public partial class Theme
+    {
+
+        public System.Int32 Id { get; set; }
+
+        public System.String Title { get; set; }
+
+        public System.String CodeExample { get; set; }
+
+        public System.String Description { get; set; }
+
+        public System.String ShortDescription { get; set; }
+
+        
+    }
+}
